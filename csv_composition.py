3908d0d6324dc523fd671a5e67ac0bf84eb5ca51
jkatsioloudes <jo.katsioloudes@gmail.com>
2019-04-17 15:51:48 +0100

    csv order test done

--- csv_composition.py
+++ csv_composition.py
@@ -2,6 +2,10 @@ import csv
 
 a = {'one': 95, 'two': 1800, 'three': 8987123}
 
+print("")
+
+a["apple"] = 1
+
 with open('test.csv', 'w') as f:
     w = csv.DictWriter(f, a.keys())
     w.writeheader()
